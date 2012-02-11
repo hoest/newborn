479c2816b819ed9a75f045dd72fe561271a10459
Jelle de Jong <jelle@hoest.nl>
2012-02-11 16:55:29 +0100

    PivotX thema

---
+++ templates/newborn/js/newborn.js
@@ -0,0 +1,28 @@
+var openArticleId = null;
+
+$(function() {
+  $('section.extra section.text').hide();
+  $('section.more a').click(function() {
+    var myId = $(this).parents('article:first').attr('id');
+
+    if(openArticleId != null) {
+      // close
+      $('#' + openArticleId + ' section.text').slideToggle();
+      $('#' + openArticleId).removeClass('border-bottom');
+    }
+
+    if(openArticleId != myId) {
+      // open
+      var article = $(this).parents('article:first');
+      $(article).find('section.text').slideToggle();
+      $(article).addClass('border-bottom');
+      $('html, body').animate(
+        { scrollTop: $(this).offset().top },
+        { duration: 'slow', easing: 'swing'}
+      ); // scroll to element
+      openArticleId = myId;
+    } else {
+      openArticleId = null;
+    }
+  });
+});
